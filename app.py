7d4c1bcff0516f7311598a43c62a52c935e97f10
Halz Man <halzman@kmbywtr-mhmwl-Halz.local>
2026-04-03 00:08:59 +0300

    fix app code

---
+++ app.py
@@ -0,0 +1,10 @@
+import streamlit as st
+import streamlit as st
+
+st.title("Real Estate Agent")
+st.write("Welcome to my Real Estate App!")
+import streamlit as st
+
+st.title("Real Estate Agent")
+st.write("Welcome to my Real Estate App!")
+
